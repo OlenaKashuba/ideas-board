c75be4d463e8130a2cb0534f22ba9336f3e4b840
OlenaKashuba <amely.jazz@gmail.com>
2018-02-25 22:37:44 +0000

    Add a test fetch for a board page

--- static/client.js
+++ static/client.js
@@ -17,12 +17,37 @@ function displayIdeas(ideas) {
 	});
 };
 
+function displayCards(cards) {
+	const cardsContainer = document.querySelector('.cards-container');
+	cards.forEach(function(item) {
+		const cardItem = document.createElement('article');
+		cardItem.setAttribute('class','card');
+		cardItem.innerHTML = `
+			<p>${item.title}</p>
+			<img width="150" height="150" src="${item.pic_url}">
+		`;
+		console.log(cardItem);
+	cardsContainer.appendChild(cardItem);
+	});
+}
+
 fetch('/api/ideas')
 	.then(function(response) {
 		return response.json();
 	})
 	.then(displayIdeas)
+	.then(displayCards)
 	.catch(function(error) {
 		return error;
 	})
+
+fetch('/api/ideas')
+	.then(function(response) {
+		return response.json();
+	})
+	.then(displayCards)
+	.catch(function(error) {
+		return error;
+	})
+
 
