8214c59b17472ec4bfe1fc142054a78d5196070c
OlenaKashuba <amely.jazz@gmail.com>
2018-02-28 13:54:01 +0000

    Fetch picture from external api to use in cards

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 const pgp = require('pg-promise')();
+const fetch = require('node-fetch');
 const app = express();
 
 app.engine(
@@ -15,6 +16,7 @@ app.engine(
 app.set('view engine', 'hbs');
 app.use(express.static('static'));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const db = pgp({
 	host: 'localhost',
@@ -30,8 +32,10 @@ app.get('/ideas-board', (req,res) => {
 	res.render('board');
 });
 
+
+
 app.get('/api/ideas', (req,res) => {
-	db.any('SELECT * FROM ideas')
+	db.any('SELECT * FROM example_ideas ORDER BY id')
 	.then((data) => {
 		return res.json(data);
 	})
@@ -39,6 +43,52 @@ app.get('/api/ideas', (req,res) => {
 		res.status(404).end();
 	})
 });
+
+// app.post('/api/ideas', (req,res) => {
+// 	db.any('INSERT INTO board(title,pic_url) SELECT title,pic_url FROM example_idea WHERE title = 'req.title' returning idea_id'
+
+// })
+
+app.post('/ideas-board', (req,res) => {
+	getImages(req.body.idea)
+	.then(function(image_url){
+		const {idea} = req.body;
+		return db.one('INSERT INTO board(title,pic_url) VALUES($1,$2) RETURNING idea_id', [idea, image_url]);
+	})
+  .then(data => {
+    res.json(Object.assign({}, {id:data.idea_id}, req.body));
+  })
+  .catch(error => {
+    res.status(404).json({
+      error: error.message
+    });
+  });
+    res.redirect('/');
+});
+
+function getImages(idea) {
+	return fetch('https://api.unsplash.com//search/photos?client_id=6a6ede767bc379254117550b04b267f5cb3e64948f0ce78bddb852a5fd23b862&query=' + idea)
+	.then(function(response) {
+		return response.json();
+	})
+	.then(body => {
+		return body.results[5].urls.small;
+	})
+	.catch(function(error) {
+		return error;
+	})
+}
+
+app.get('/api/ideas-board', (req,res) => {
+	db.any('SELECT * FROM board') 
+	.then((data) => {
+		return res.json(data);
+	})
+	.catch((error) => {
+		res.status(404).end();
+	})
+});
+
 
 
 
--- static/client.js
+++ static/client.js
@@ -1,23 +1,41 @@
+const selectedIdeasArray= []
+
 function displayIdeas(ideas) {
 	const ideasContainer = document.querySelector('.examples-container');
 	const ideasList = document.createElement('ol');
 	ideas.forEach(function(idea) {
 		const ideasListItem = document.createElement('li');
 		ideasListItem.innerHTML = `
-			<span>${idea.title}<span>
-			<button class="add-button"> Add </button>
+			<span>${idea.title} 
+				<button class="add-button"> Add </button>
+			<span>
+			
 		`;
 		ideasList.appendChild(ideasListItem);
 
 	});
 	ideasContainer.appendChild(ideasList);
-	const addButton = document.querySelector('.add-button');
-	addButton.addEventListener('click', function(event) {
- 		debugger;
-	});
+
+	const addButtons = document.querySelectorAll('.add-button');
+		addButtons.forEach(function(button) {
+			button.addEventListener('click', function(e) {
+				button.disabled = 'true';
+				console.log(e.target.previousSibling.data.trim());
+				return (e.target.previousSibling.data).trim();
+				// selectedIdeasArray.push((e.target.previousSibling.data).trim());
+				
+				// console.log(selectedIdeasArray);
+			});	
+		});
 };
 
+
 function displayCards(cards) {
+	fetch('/api/ideas-board')
+	.then(function(response) {
+		return response.json();
+	})
+	.then
 	const cardsContainer = document.querySelector('.cards-container');
 	cards.forEach(function(item) {
 		const cardItem = document.createElement('article');
@@ -26,8 +44,21 @@ function displayCards(cards) {
 			<p>${item.title}</p>
 			<img width="150" height="150" src="${item.pic_url}">
 		`;
-		console.log(cardItem);
 	cardsContainer.appendChild(cardItem);
+	});
+}
+
+function displayList(ideas) {
+	const listContainer = document.querySelector('.selected-ideas-list');
+	ideas.forEach(function(idea) {
+		const listItem = document.createElement('li');
+		listItem.setAttribute('class', 'list-item');
+		listItem.innerHTML =`
+			<p>${item.title}</p>
+		`;
+		console.log(listItem);
+		listContainer.appendChild(listItem);
+		
 	});
 }
 
@@ -36,12 +67,12 @@ fetch('/api/ideas')
 		return response.json();
 	})
 	.then(displayIdeas)
-	.then(displayCards)
 	.catch(function(error) {
 		return error;
 	})
 
-fetch('/api/ideas')
+
+fetch('/api/ideas-board')
 	.then(function(response) {
 		return response.json();
 	})
